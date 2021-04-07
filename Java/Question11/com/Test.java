89175f044b1c026e8544d21bb83effe422e9f966
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:32:41 +0530

    Test

---
+++ Java/Question11/com/Test.java
@@ -0,0 +1,11 @@
+package com;
+import com.loan.*;
+import com.transact.*;
+
+
+public class Test {
+	public static void main(String[] args) {
+		LoanAccount la =new LoanAccount();
+		la.doTransaction();
+	}
+}
