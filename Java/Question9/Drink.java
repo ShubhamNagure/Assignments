61c9c12eaecb5017d20b5e515b8ae7c0c594a864
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:16:37 +0530

    Drink

---
+++ Java/Question9/Drink.java
@@ -0,0 +1,12 @@
+public class Drink extends Product {
+	
+	Drink(){
+		displayDrink();
+	}
+	
+	public void displayDrink() {
+		// TODO Auto-generated method stub
+		System.out.println("This is display Drink method");
+	}
+
+}
