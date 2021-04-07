13196d3480cce85f2fbe7eb79afba59c1bad3e4b
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:25:32 +0530

    updated product manager as per q10

--- Java/Question9/ProductManager.java
+++ Java/Question9/ProductManager.java
@@ -8,5 +8,25 @@ public class ProductManager extends Shop{
 	public Product proDrink() {
 		return new Drink();
 	}
+	//this method is subject of 10 question only : is has nothing to do with q9
+	public Review rateAndReview(String r) {
+		Review rev = null;
+		if (r=="X") {
+			new Review().oneStar();
+		} else if (r=="XX") {
+			new Review().twoStar();
+		} else if (r=="XXX") {
+			new Review().threeStar();
+		} else if (r== "XXXX") {
+			new Review().fourStar();
+		}else if (r == "XXXXX") {
+			new Review().fiveStar();
+		}else {
+			System.out.println("Please rate now !");
+		}
+		
+		
+		return null;
+	}
   
 }
