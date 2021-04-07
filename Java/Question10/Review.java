b5d6f39445e5b02d139e158ca25b3accd2c1324c
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:23:23 +0530

    Review.java

---
+++ Java/Question10/Review.java
@@ -0,0 +1,40 @@
+public class Review implements Rateable {
+
+	
+	
+	@Override
+	public void oneStar() {
+		// TODO Auto-generated method stub
+		System.out.println("Thanks you're rating is X");
+		
+	}
+
+	@Override
+	public void twoStar() {
+		// TODO Auto-generated method stub
+		System.out.println("Thanks you're rating is X X");
+	}
+
+	@Override
+	public void threeStar() {
+		// TODO Auto-generated method stub
+		System.out.println("Thanks you're rating is X X X");
+		
+	}
+
+	@Override
+	public void fourStar() {
+		// TODO Auto-generated method stub
+		System.out.println("Thanks you're rating is X X X X");
+	}
+
+	@Override
+	public void fiveStar() {
+		// TODO Auto-generated method stub
+		System.out.println("Thanks you're rating is X X X X X");
+	}
+
+	
+	
+
+}
