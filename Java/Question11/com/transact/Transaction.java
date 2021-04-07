3ec2b4b1d249770d398632d82e14d15dbd2e86b2
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:32:01 +0530

    Transaction

---
+++ Java/Question11/com/transact/Transaction.java
@@ -0,0 +1,12 @@
+package com.transact;
+
+public class Transaction {
+	
+	public static void credit() {
+		System.out.println("Credited");
+	}
+	
+	public static void debit() {
+		System.out.println("Debited");
+	}
+}
