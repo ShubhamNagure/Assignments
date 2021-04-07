c1ff3b256b41fe9fcd379b88ed58f9c5b3679cbc
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:15:57 +0530

    Food.java

---
+++ Java/Question9/Food.java
@@ -0,0 +1,12 @@
+public class Food extends Product{
+	
+	Food(){
+		displayFood();
+	}
+	
+	public void displayFood() {
+		// TODO Auto-generated method stub
+		System.out.println("This is display food method");
+	}
+
+}
