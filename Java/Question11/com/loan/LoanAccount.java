206ad45b727ebdfffc7b8b1b2a3c134a985d5d2c
Shubham Nagure <49149160+ShubhamNagure@users.noreply.github.com>
2021-04-07 12:30:53 +0530

    LoanAccount

---
+++ Java/Question11/com/loan/LoanAccount.java
@@ -0,0 +1,13 @@
+package com.loan;
+
+import com.transact.Transaction;
+
+public class LoanAccount {
+	
+	
+	public void doTransaction() {
+		// TODO Auto-generated method stub
+		Transaction.credit();
+		Transaction.debit();
+	}
+}
