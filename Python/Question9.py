96d1ff53e5aa5742e2a02d4cd076f1036c18dc18
ShubhamNagure <nagure.shubham@gmail.com>
2021-07-08 17:10:31 +0530

    all prime numbers less than the entered number

---
+++ Python/Question9.py
@@ -0,0 +1,25 @@
+# 9.	Find all prime numbers less than the entered number
+def isPrime(n):
+     
+    # Corner case
+    if n <= 1 :
+        return False
+ 
+    # check from 2 to n-1
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+ 
+    return True
+ 
+# Function to print primes
+def printPrime(n):
+    for i in range(2, n + 1):
+        if isPrime(i):
+            print(i, end = " ")
+ 
+# Driver code
+if __name__ == "__main__" :
+    n =int(input("Enter A number :"))
+    # function calling
+    printPrime(n)
