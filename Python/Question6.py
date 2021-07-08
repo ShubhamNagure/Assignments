205e6220a6a97a3e4c1d6c13389ff43d9fca854d
ShubhamNagure <nagure.shubham@gmail.com>
2021-07-08 13:06:56 +0530

    number of digits in the given number

---
+++ Python/Question6.py
@@ -0,0 +1,19 @@
+# 6.	Find the number of digits in the given number
+
+def easyLen(n):
+    """Easy Method"""
+    print(len(n))
+
+def lenOfDigits(n):
+    """With While loop"""
+    i = 0
+    flag=True
+    while n != 0:
+        n = n//10
+        i +=1
+    print(i)
+
+
+if __name__ == "__main__":
+    n=input('Enter a number:')
+    lenOfDigits(int(n))
