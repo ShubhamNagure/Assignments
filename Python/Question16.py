eaec3521ee559bc9b1ff52d4718b099c5067e834
ShubhamNagure <nagure.shubham@gmail.com>
2021-07-25 09:37:13 +0530

    Write a program to accept the positive integer n # from the user and print counting of numbers which are # not prime from 1 to n

---
+++ Python/Question16.py
@@ -0,0 +1,38 @@
+# 16.	Write a program to accept the positive integer n 
+# from the user and print counting of numbers which are 
+# not prime from 1 to n.
+
+
+from typing import Counter
+
+
+def positiveIntegerOnly():
+    goodinput = False
+    while not goodinput:
+        try:
+            number = int(input('Enter a number: '))
+            if number > 0:
+                goodinput = True
+                return number
+            else:
+                print("that's not a positive number. Try again: ")
+        except ValueError:
+            print("that's not an integer. Try again: ")
+
+def countNonPrime(upper):
+    lower = 1
+    nonPrime=[0, 1]
+    primeList=[]
+    for num in range(lower,upper + 1):  
+        if num > 1:  
+            for i in range(2,num):  
+                if (num % i) == 0:
+                    nonPrime.append(num)  
+                    break  
+            else:
+                primeList.append(num)  
+                # print(num)
+    print(len(nonPrime))
+if __name__ == "__main__":
+    num=positiveIntegerOnly()
+    countNonPrime(num)
