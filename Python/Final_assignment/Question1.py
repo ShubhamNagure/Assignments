18604da573d1a5650bc5fe7bd10b826dad8cc8af
ShubhamNagure <nagure.shubham@gmail.com>
2021-09-08 14:20:28 +0530

    transpose matrix and pattern program

---
+++ Python/Final_assignment/Question1.py
@@ -0,0 +1,41 @@
+# Write a Python Program program 
+# to Transpose a Matrix
+# to construct the following pattern, using a nested for loop
+
+"""
+to Transpose a Matrix
+
+"""
+
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+
+for r in result:
+   print(r)
+
+   """
+   to construct the following pattern, using a nested for loop
+   """
+
+
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+
